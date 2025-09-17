b7c274c01c56743214c418aaefe54aa8bc83d39e
nobuhiro-sato <n.s.mh256@gmail.com>
2025-09-18 05:36:31 +0900

    add:フォント設定追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import Header from '@/components/header'
+import { Noto_Sans_JP } from 'next/font/google'
+
+const notoSansJapanese = Noto_Sans_JP({
+  weight: '700',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: 'きのこの山岳',
@@ -13,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html data-theme="retro" lang="ja">
+    <html data-theme="retro" lang="ja" className={notoSansJapanese.className}>
       <Header />
       <body>{children}</body>
     </html>
